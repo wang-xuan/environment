6f0b64e1d34e1a447c5bbfbbc4bff22e7b2e6993
wx <15038713379@163.com>
2019-11-13 11:11:55 +0800

    test byName autowired : 将与bean属性名字相同的 其他beanName自动装配到该bean对应的属性值中

--- src/main/java/com/itcode/environment/spring/mode/School.java
+++ src/main/java/com/itcode/environment/spring/mode/School.java
@@ -8,6 +8,9 @@ public class School {
     private String schoolName;
     private String address;
     private int studentNumber;
+    /**byName：按照名字自动装配即 将与bean中[属性名字相同]的 其他[beanName]自动装配到该Bean对应的属性中*/
+    private Student student;
+
     public String getSchoolName() {
         return schoolName;
     }
@@ -26,6 +29,15 @@ public class School {
     public void setStudentNumber(int studentNumber) {
         this.studentNumber = studentNumber;
     }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
     @Override
     public String toString() {
         return "School [schoolName=" + schoolName + ", address=" + address
--- src/main/java/com/itcode/environment/spring/test/FactoryBeanTest.java
+++ src/main/java/com/itcode/environment/spring/test/FactoryBeanTest.java
@@ -1,6 +1,7 @@
 package com.itcode.environment.spring.test;
 
 import com.itcode.environment.spring.factorybean.FactoryBeanPojo;
+import com.itcode.environment.spring.mode.School;
 import com.itcode.environment.spring.mode.Student;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,8 +14,13 @@ public class FactoryBeanTest {
         ClassPathXmlApplicationContext pathXmlApplicationContext = new ClassPathXmlApplicationContext("BeanConfig.xml");
         Student student = (Student) pathXmlApplicationContext.getBean("student");
         System.out.println(student.getName());
+        System.out.println("-----------factoryBean实现类getBean--------");
         FactoryBeanPojo factoryBeanPojo = (FactoryBeanPojo) pathXmlApplicationContext.getBean("&factoryBeanPojo");
         System.out.println(factoryBeanPojo.getClass().getName());
         System.out.println("type:-" + factoryBeanPojo.getType());
+        System.out.println("------------自动装配属性值-------");
+        //采用byName方式，自动装配School中的student属性值
+        School school = (School) pathXmlApplicationContext.getBean("school");
+        System.out.println("school:" + school.getSchoolName() + ", studentName:" + school.getStudent().getName());
     }
 }
